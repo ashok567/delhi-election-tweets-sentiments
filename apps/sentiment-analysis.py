7b847ba753414a5afb2882bb461ecdef8fac0740
Ashok Thakur <iashok180@gmail.com>
2020-02-07 23:50:47 +0530

    training model sample added

--- apps/sentiment-analysis.py
+++ apps/sentiment-analysis.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
 
 
 def tokenization_tweets(dataset, features):
@@ -10,14 +14,49 @@ def tokenization_tweets(dataset, features):
     return dataset_transformed
 
 
+def train(X_train_mod, y_train, features, shuffle, drop, layer1, layer2, epoch, lr, epsilon, validation):
+    model_nn = Sequential()
+    model_nn.add(Dense(layer1, input_shape=(features,), activation='relu'))
+    model_nn.add(Dropout(drop))
+    model_nn.add(Dense(layer2, activation='sigmoid'))
+    model_nn.add(Dropout(drop))
+    model_nn.add(Dense(3, activation='softmax'))
+
+    optimizer = keras.optimizers.Adam(lr=lr, beta_1=0.9, beta_2=0.999, epsilon=epsilon, decay=0.0, amsgrad=False)
+    model_nn.compile(loss='sparse_categorical_crossentropy',
+                     optimizer=optimizer,
+                     metrics=['accuracy'])
+    model_nn.fit(np.array(X_train_mod), y_train,
+                 batch_size=32,
+                 epochs=epoch,
+                 verbose=1,
+                 validation_split=validation,
+                 shuffle=shuffle)
+    return model_nn
+
+
+def model1(X_train, y_train):
+    features = 100
+    shuffle = True
+    drop = 0.5
+    layer1 = 512
+    layer2 = 256
+    epoch = 2
+    lr = 0.001
+    epsilon = None
+    validation = 0.1
+    X_train_mod = tokenization_tweets(X_train, features)
+    model = train(X_train_mod, y_train, features, shuffle, drop, layer1, layer2, epoch, lr, epsilon, validation)
+    return model
+
+
 def main():
     df = pd.read_csv('data.csv')
     df = df.dropna()
     X_train, X_test, y_train, y_test = train_test_split(
             df['tweet'], df['sentiment'], test_size=0.2, shuffle=True)
 
-    X_train_mod = tokenization_tweets(X_train, 3500)
-    print(X_train_mod)
+    model1(X_train, y_train)
 
 
 if __name__ == "__main__":
